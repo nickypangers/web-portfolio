4f807df1e41cee3658d62931b617485d0671797b
Nixon <nixon.p@mosaicdigi.com>
2021-09-01 17:21:44 +0800

    updated about and text color

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,16 +2,17 @@
   <div id="about" class="section about container text-start">
     <h2>About Me</h2>
     <p>
-      Hi! I am Nixon, and I build websites and applications for a living. My
-      interest in programming dated back in 2006 when my sister introduced me to
-      xanga. My first time coding was when I tried to customise the cursor of my
-      blog using CSS, and I have not stopped learning how to code ever since!
+      Hi! I am Nixon and I build websites and applications for a living. My
+      interest in programming dated back to 2006 when my sister introduced me to
+      xanga. The first time I had a go at coding was when I tried to customise
+      the cursor of my blog using CSS, and I have not stopped learning how to
+      code ever since!
     </p>
     <p>
-      Today, I am currently a
+      Today, I work as a
       <span class="highlight">Web/Mobile App Developer</span> working at a
       digital agency based in Hong Kong, where my colleagues and I create
-      websites and mobile applications according to the client's needs.
+      websites and mobile applications tailored to our clients' needs.
       <span class="font-normal italic"
         >Prior to this position, I was a hardware engineer.</span
       >
