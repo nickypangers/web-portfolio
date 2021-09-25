52a303d7617a717f902d87d762bcd63332ce9ae3
nickypangers <nixonpang98@gmail.com>
2021-09-26 03:05:54 +0800

    tailwind

--- src/main.js
+++ src/main.js
@@ -1,28 +1,6 @@
 import { createApp } from "vue";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faGooglePlay,
-  faGithub,
-  faLinkedinIn,
-  faStackOverflow,
-} from "@fortawesome/free-brands-svg-icons";
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import App from "./App.vue";
 import router from "./router";
+import "./assets/tailwind.css";
 
-library.add(faGooglePlay);
-library.add(faGithub);
-library.add(faLinkedinIn);
-library.add(faStackOverflow);
-library.add(faExternalLinkAlt);
-
-createApp(App)
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .use(router)
-  .mount("#app");
+createApp(App).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,12 @@
 <template>
   <div class="home">
-    <Banner />
-    <About />
-    <Jobs />
-    <Projects />
-    <GetInTouch />
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
 <script>
-import Banner from "@/components/Banner.vue";
-import About from "@/components/About.vue";
-import Jobs from "@/components/Jobs.vue";
-import Projects from "@/components/Projects.vue";
-import GetInTouch from "@/components/GetInTouch.vue";
-
 export default {
   name: "Home",
-  components: {
-    Banner,
-    About,
-    Jobs,
-    Projects,
-    GetInTouch,
-  },
+  components: {},
 };
 </script>
-
-<style lang="scss" scoped>
-@media (min-width: 992px) {
-  .home {
-    margin-bottom: 0 !important;
-  }
-}
-
-.home {
-  position: relative;
-  margin-bottom: $footer-height;
-}
-</style>
