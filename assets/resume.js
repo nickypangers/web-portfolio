870fcf7008d96085b0a448ae2f3cd1ee8a057cad
nickypangers <nixonpang98@gmail.com>
2022-08-11 00:20:49 +0800

    update resume

--- assets/resume.js
+++ assets/resume.js
@@ -52,12 +52,26 @@ export const resumeData = {
   education: education,
   experiences: [
     {
+      company: 'Chaos Theroy',
+      position: 'Full Stack Developer',
+      period: 'May 2022 - Present',
+      location: 'Central, Hong Kong',
+      description:
+        'I am working at Chaos Theory as a Full Stack Developer to develop bespoke functionality for an in-house sports news app for both Android and iOS.',
+      points: [
+        'Develop bespoke functionality on the app, usually totally unique on a sport-by-sport basis',
+        'Own various parts of our data pipeline that may come via web-scraping techniques',
+        'Set, report and communicate OKRs for your piece of the project',
+        'Own the website infrastructure, deployment pipeline and performance monitoring, including the cost profile',
+      ],
+    },
+    {
       company: 'AQUMON',
       position: 'Software Developer',
-      period: 'Jan 2022 - Present',
+      period: 'Jan 2022 - May 2022',
       location: 'Quarry Bay, Hong Kong',
       description:
-        'I am working at Aqumon as a Software Developer to create and maintain frontend modules for asset management, and interactive websites for data analysis using various chart libraries',
+        'I worked at Aqumon as a Software Developer to create and maintain frontend modules for asset management, and interactive websites for data analysis using various chart libraries',
       points: [
         'Develop, implement and maintain frontend modules for asset management for middle and backend office',
         'Delivery interactive websites to explain quantitative research ideas according to the product team',
@@ -81,7 +95,7 @@ export const resumeData = {
     {
       company: 'SG Wireless',
       position: 'Hardware Engineer',
-      period: 'Feb 2020 - Oct 2020',
+      period: 'Feb 2020 - Sep 2020',
       location: 'Tsuen Wan, Hong Kong',
       description:
         'I worked at SG Wireless as a Hardware Engineer to develop internet-of-things products involving BLE, LoRa and WiFi 6 technologies.',
@@ -91,7 +105,7 @@ export const resumeData = {
         'Developing test programs using C, C#, Go and LabView',
         'Quality assurance of hardware function design',
         'Compliance and product certification',
-        'Translate customer product requirements into hardware requirements, test plan and design documentations before hardwrae implementation',
+        'Translate customer product requirements into hardware requirements, test plan and design documentations before hardware implementation',
         'In-house manufacturing setup',
       ],
     },
